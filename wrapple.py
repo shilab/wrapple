a7a6f35b7d59c23a51f7e383a820b8e03da1ddb4
Andrew Quitadamo <aquitada@uncc.edu>
2014-11-11 12:05:17 -0500

    Added status checking. Closes #2 and #3

--- wrapple.py
+++ wrapple.py
@@ -1,12 +1,29 @@
 import urllib, urllib2
+import re
+import time
+
+def check_status():
+    status_page = urllib2.urlopen(link) 
+    status_page_list = status_page.read().split("\n")
+
+    for line in status_page_list:
+        if 'status' in line:
+            t= line.split("<BR>")
+            print t[2] + "\t" + t[3].split("<a href")[0]
+            if 'FINISHED' in t[2]:
+                for l in status_page_list:
+                    if '_summary' in l:
+                        print l
+            else:
+                time.sleep(60)
+                return check_status()
+
 page = 'http://www.broadinstitute.org/mpg/dapple/dappleTMP.php'
-#headers = {'':''}
 raw_params = {'genome':'19','numberPermutations':'1000','CIcutoff':'2','regUp':'50','regDown':'50','snpListFile':'filename=""','snpList':"BRCA1\nBRCA2",'genesToSpecifyFile':'filename=""','genesToSpecify':'','zoomedGenes':'','email':'aquitada@uncc.edu','description':'DappleTest','submit':'submit'}
 params = urllib.urlencode(raw_params)
 request = urllib2.Request(page, params)
 page = urllib2.urlopen(request)
 info = page.info()
-#print page.read() 
 page_list = page.read().split("\t")
 
 for line in page_list:
@@ -14,3 +31,5 @@ for line in page_list:
             link = line.split("<a href=")[1].split(">")[0]
                     
 print "The link to the results is: " + link
+
+check_status()
