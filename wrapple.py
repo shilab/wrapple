c8921cc5d1bb0f3aec69a58784344e0f44a4edf3
Andrew Quitadamo <aquitada@uncc.edu>
2014-11-12 06:04:18 -0500

    Added dropped connection handling. Closes #9

--- wrapple.py
+++ wrapple.py
@@ -2,10 +2,28 @@ import urllib, urllib2
 import re
 import time
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-e','--email', nargs=1) 
+parser.add_argument('-d','--description',nargs=1)
+parser.add_argument('-f','--snpfile',nargs=1)
+args = parser.parse_args()
+
+email = args.email[0]
+description=args.description[0]
+filename = args.snpfile[0]
+
+snp_file = open(filename,'r')
+snps =snp_file.read()
 
 def check_status():
-    status_page = urllib2.urlopen(link) 
-    status_page_list = status_page.read().split("\n")
+    try:
+        status_page = urllib2.urlopen(link) 
+        status_page_list = status_page.read().split("\n")
+    except:
+        time.sleep(60)
+        return check_status()
 
     for line in status_page_list:
         if 'status' in line:
@@ -19,7 +37,11 @@ def check_status():
                             if 'http:' in t:
                                 file =t.split("/")[-1].split("\"")[0] 
                                 #command = 'wget ' + t.split("\"")[1]
-                                command = 'curl ' + t.split("\"")[1] + '>' + file
+                                #TODO:Check if directory exists before mkdir
+                                make_dir = 'mkdir ' + description
+                                os.system(make_dir)
+                                file = description+'/'+file
+                                command = 'curl ' + t.split("\"")[1] + '>' +file 
                                 os.system(command)
                         
             else:
@@ -27,7 +49,7 @@ def check_status():
                 return check_status()
 
 page = 'http://www.broadinstitute.org/mpg/dapple/dappleTMP.php'
-raw_params = {'genome':'19','numberPermutations':'1000','CIcutoff':'2','regUp':'50','regDown':'50','snpListFile':'filename=""','snpList':"BRCA1\nBRCA2",'genesToSpecifyFile':'filename=""','genesToSpecify':'','zoomedGenes':'','email':'andrew.quitadamo@gmail.com','description':'DappleTest','submit':'submit'}
+raw_params = {'genome':'19','numberPermutations':'1000','CIcutoff':'2','regUp':'50','regDown':'50','snpListFile':'filename=""','snpList':snps,'genesToSpecifyFile':'filename=""','genesToSpecify':'','zoomedGenes':'','email':email,'description':description,'submit':'submit'}
 params = urllib.urlencode(raw_params)
 request = urllib2.Request(page, params)
 page = urllib2.urlopen(request)
@@ -35,8 +57,8 @@ page_list = page.read().split("\t")
 
 for line in page_list:
     if 'status' in line:
-            link = line.split("<a href=")[1].split(">")[0]
-                    
+        link = line.split("<a href=")[1].split(">")[0]
+            
 print "The link to the status page and results is: " + link
 
 check_status()
