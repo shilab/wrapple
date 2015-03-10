e8805baae2d7e6c6b32588491fd8c821f8480a67
Andrew Quitadamo <aquitada@uncc.edu>
2015-03-10 14:29:31 -0400

    Removed wrapple directory

--- test/test_wrapple.py
+++ test/test_wrapple.py
@@ -31,12 +31,12 @@ def test_check_args_7():
 def test_get_snps_1():
     get_snps('not_a_snp_file')
 def test_get_snps_2():
-    assert get_snps('wrapple/test/resources/snps') == 'rs3890745\nrs2240340\nrs2476601'
+    assert get_snps('test/resources/snps') == 'rs3890745\nrs2240340\nrs2476601'
 
 def test_get_zoomed_genes_1():
     assert get_zoomed_genes(None) == ''
 def test_get_zoomed_genes_2():
-    assert get_zoomed_genes('wrapple/test/resources/zoom_gene') == 'BRCA1,BRCA2'
+    assert get_zoomed_genes('test/resources/zoom_gene') == 'BRCA1,BRCA2'
 @raises(SystemExit)
 def test_get_zoomed_genes_3():
     get_zoomed_genes('not_a_zoom_file')
@@ -44,7 +44,7 @@ def test_get_zoomed_genes_3():
 def test_get_specified_genes_1():
     assert get_specified_genes(None) == ''
 def test_get_specified_genes_2():
-    assert get_specified_genes('wrapple/test/resources/specified') == 'BRCA1\nBRCA2'
+    assert get_specified_genes('test/resources/specified') == 'BRCA1\nBRCA2'
 @raises(SystemExit)
 def test_get_specified_genes_3():
     get_specified_genes('not_a_specified_file')
@@ -58,29 +58,29 @@ def test_create_request_1():
 @raises(SystemExit)
 def test_create_request_2():
     parser = create_parser()
-    args = parser.parse_args(['-f', 'wrapple/test/resourcessnps'])
+    args = parser.parse_args(['-f', 'test/resourcessnps'])
     create_request(args)
 
 @raises(SystemExit)
 def test_create_request_3():
     parser = create_parser()
-    args = parser.parse_args(['-f', 'wrapple/test/resources/snps', '-d', 'description'])
+    args = parser.parse_args(['-f', 'test/resources/snps', '-d', 'description'])
     create_request(args)
 
 class TestRequest(unittest.TestCase):
     def test_create_request_4(self):
         parser = create_parser()
-        args = parser.parse_args(['-f', 'wrapple/test/resources/snps', '-d', 'description', '-e', 'test@test'])
+        args = parser.parse_args(['-f', 'test/resources/snps', '-d', 'description', '-e', 'test@test'])
         params, _, _ = create_request(args)
         exp_param = {'plot': False, 'zoomedGenes': '', 'description': 'description', 'numberPermutations': 1000, 'snpListFile': 'filename=""', 'nearestgene': '', 'CIcutoff': 2, 'regDown': 50, 'email': 'test@test', 'submit': 'submit', 'genome': '19', 'regUp': 50, 'snpList': 'rs3890745\nrs2240340\nrs2476601', 'collapseCI': False, 'plotP': False, 'genesToSpecify': ''}
         self.assertEqual(params, exp_param)
 
 def fail_urlopen(url):
-    url_file = open('wrapple/test/resources/fail.html', 'rb')
+    url_file = open('test/resources/fail.html', 'rb')
     return url_file
 
 def status_urlopen(url):
-    url_file = open('wrapple/test/resources/status.html', 'rb')
+    url_file = open('test/resources/status.html', 'rb')
     return url_file
 
 class sendFail(unittest.TestCase):
@@ -128,7 +128,7 @@ class SendException(unittest.TestCase):
         _, _, _ = send_parameters(request, 1, 'description')
 
 def finished_urlopen(url):
-    url_file = open('wrapple/test/resources/finished.html', 'rb')
+    url_file = open('test/resources/finished.html', 'rb')
     return url_file
 
 class StatusFinished(unittest.TestCase):
@@ -148,7 +148,7 @@ class StatusFinished(unittest.TestCase):
         assert commands[0] == 'curl http://www.broadinstitute.org/mpg/dapple/results/1418998161/StatusTest_summary -o description/StatusTest_summary'
 
 def run_urlopen(url):
-    url_file = open('wrapple/test/resources/run.html', 'rb')
+    url_file = open('test/resources/run.html', 'rb')
     return url_file
 
 class StatusRun(unittest.TestCase):
@@ -163,7 +163,7 @@ class StatusRun(unittest.TestCase):
         assert check_status('link', 1, 'description') == False
 
 def pend_urlopen(url):
-    url_file = open('wrapple/test/resources/pend.html', 'rb')
+    url_file = open('test/resources/pend.html', 'rb')
     return url_file
 
 class StatusPend(unittest.TestCase):
